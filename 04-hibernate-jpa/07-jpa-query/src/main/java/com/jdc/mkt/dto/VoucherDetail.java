e58432a72cb77a929f7f7d62f39dac27146e548b
uthuyein <uthuyeinlinn@gmail.com>
2025-09-14 09:03:48 +0630

    update data

--- 04-hibernate-jpa/07-jpa-query/src/main/java/com/jdc/mkt/dto/VoucherDetail.java
+++ 04-hibernate-jpa/07-jpa-query/src/main/java/com/jdc/mkt/dto/VoucherDetail.java
@@ -3,10 +3,12 @@ package com.jdc.mkt.dto;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "voucher_detail_tbl")
 public class VoucherDetail {
 
 	@EmbeddedId
@@ -16,9 +18,7 @@ public class VoucherDetail {
 	private double subTotal;
 	
 	@ManyToOne
-	//@JoinColumn(insertable = false,updatable = false)
 	private Product product;
 	@ManyToOne
-	//@JoinColumn(insertable = false,updatable = false)
 	private Voucher voucher;
 }

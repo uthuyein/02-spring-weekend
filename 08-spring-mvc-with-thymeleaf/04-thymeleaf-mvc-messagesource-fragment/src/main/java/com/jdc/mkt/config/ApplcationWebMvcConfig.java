e7052f9a639eb4a21e04b1a6024bdd2378875d4f
uthuyein <uthuyeinlinn@gmail.com>
2025-12-06 11:24:38 +0630

    update

--- 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/config/ApplcationWebMvcConfig.java
+++ 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/config/ApplcationWebMvcConfig.java
@@ -1,14 +1,23 @@
 package com.jdc.mkt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.jdc.mkt.controller")
 public class ApplcationWebMvcConfig implements WebMvcConfigurer{
 
+	@Autowired
+	private LocaleChangeInterceptor interceptor;
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor);
+	}
 }
--- 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/config/MessageSourceConfig.java
+++ 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/config/MessageSourceConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MessageSourceConfig {
@@ -14,4 +16,18 @@ public class MessageSourceConfig {
 		bean.addBasenames("languages/message");
 		return bean;
 	}
+	
+	@Bean
+	SessionLocaleResolver localeResolver() {
+		var bean = new SessionLocaleResolver();
+		return bean;
+	}
+	
+	@Bean
+	LocaleChangeInterceptor changeInterceptor() {
+		var bean = new LocaleChangeInterceptor();
+		bean.setParamName("lang");
+		return bean;
+	}
+	
 }

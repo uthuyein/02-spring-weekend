af55304201b5da4adbc7ccb62b7bf194b4a5c646
uthuyein <uthuyeinlinn@gmail.com>
2025-12-07 11:59:30 +0630

    update

--- 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/controller/HomeController.java
+++ 08-spring-mvc-with-thymeleaf/04-thymeleaf-mvc-messagesource-fragment/src/main/java/com/jdc/mkt/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.jdc.mkt.controller;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,6 +19,18 @@ public class HomeController {
 	@GetMapping
 	String index() {
 		return "home";
+	}
+	
+	@GetMapping("objects")
+	String objectTest(ModelMap map) {
+		map.put("value", "Hello Java");
+		map.put("number", 23232323);
+		map.put("decimal", 232323.4523);
+		map.put("array",new String[] {"1","2"});
+		map.put("list",null);
+		map.put("set", Set.of("Set 1","Set 2"));
+		map.put("map", Map.of(1,"One",2,"Two"));
+		return "basic-datatype";
 	}
 	
 	@GetMapping("dates")
